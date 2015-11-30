5644e58b50c06a37fcd791f3e6a2aef7af0c4772
travis-wentz <wentz.travis@gmail.com>
2015-11-30 14:36:43 -0700

    html working

--- webpage/myscripts.js
+++ webpage/myscripts.js
@@ -1,36 +1,30 @@
 function cityAndType(city, htype) {
     var text = "hi";
-    document.getElementById("error").innerHTML = city;
-    document.getElementById("error1").innerHTML = htype;
-    return;
+    // document.getElementById("error").innerHTML = city;
+    // document.getElementById("error1").innerHTML = htype;
+    // return;
 
     switch(city){
         case "Bozeman":
             switch(htype){
-                case "HouseBuy":
-                	window.alert(city);
-                    window.location = "file:///Users/TravisWentz/github/dbProject/webpage/html/houseBuy.html";
-                    //houseBuy.html;
-                    //SQL to get houses to buy in Bozeman;
-                    break;
                 case("HouseRent"):
-                    //go to houseRent.html;
+                    window.location = "file:///Users/TravisWentz/github/dbProject/webpage/html/houseRent.html";
                     //SQL to get houses to rent in Bozeman;
                     break;
                 case("Apartment"):
-                    //go to apartment.html;
+                    window.location = "file:///Users/TravisWentz/github/dbProject/webpage/html/apartment.html";
                     //SQL to get apartments in Bozeman;
                     break;
                 case("Condo"):
-                    //go to condo.html;
+                    window.location = "file:///Users/TravisWentz/github/dbProject/webpage/html/condo.html";
                     //SQL to get condos in Bozeman;
                     break;
                 case("Trailer"):
-                    //go to trailer.html;
+                    window.location = "file:///Users/TravisWentz/github/dbProject/webpage/html/trailer.html";
                     //SQL to get trailers in Bozeman;
                     break;
                 case("Land"):
-                    //go to land.html;
+                    window.location = "file:///Users/TravisWentz/github/dbProject/webpage/html/land.html";
                     //SQL to get land in Bozeman;
                     break;
                 default:
